bb05c43784e3922566338bb9fabdb6ca101e0ea9
JimmyOu <15757175841@163.com>
2018-01-18 16:59:03 +0800

    初始化

---
+++ TestPromise/TestPromise/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestPromise
+//
+//  Created by JimmyOu on 2018/1/18.
+//  Copyright © 2018年 JimmyOu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestPromise/TestPromise/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestPromise
+//
+//  Created by JimmyOu on 2018/1/18.
+//  Copyright © 2018年 JimmyOu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
